96de68b1d3eeed68deba91c0a68a599d27e05931
Ali Bozorgzadeh <abzgzh@gmail.com>
2020-12-24 02:47:28 +0330

    Extensive change in key-binding

--- config.def.h
+++ config.def.h
@@ -54,27 +54,55 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
+/* Hardware multimedia keys */
+#include <X11/XF86keysym.h>
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *web[]  = { "brave", NULL };
+static const char *mail[]  = { "thunderbird", NULL };
+static const char *filemgr[]  = { "lf", NULL };
+
+/* multimedia keys */
+static const char *upvol[]     = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[]   = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+static const char *brupcmd[]   = { "/usr/bin/xbacklight", "-inc", "10", NULL };
+static const char *brdowncmd[] = { "/usr/bin/xbacklight", "-dec", "10", NULL };
+
+/* playerctl */
+static const char *pctlpp[] = { "/usr/bin/playerctl", "--player=mpd", "play-pause", NULL };
+static const char *pctln[]  = { "/usr/bin/playerctl", "--player=mpd", "next",       NULL };
+static const char *pctlp[]  = { "/usr/bin/playerctl", "--player=mpd", "previous",   NULL };
+
+/* protonvpn */
+static const char *pvpn[]  = { "/home/ali/.local/scripts/Vpn", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = web    } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = mail   } },
+	{ MODKEY,                       XK_f,      spawn,          {.v = filemgr  } },
+    { MODKEY,                       XK_p,      spawn,          {.v = pctlpp } },
+    { MODKEY,                       XK_s,      spawn,          {.v = pctln  } },
+    { MODKEY,                       XK_a,      spawn,          {.v = pctlp  } },
+    { MODKEY|ShiftMask,             XK_v,      spawn,          {.v = pvpn   } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -94,6 +122,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { 0,                            XF86XK_AudioLowerVolume,    spawn,  {.v = downvol  } },
+    { 0,                            XF86XK_AudioMute,           spawn,  {.v = mutevol  } },
+    { 0,                            XF86XK_AudioRaiseVolume,    spawn,  {.v = upvol    } },
+    { 0,                            XF86XK_MonBrightnessUp,     spawn,  {.v = brupcmd  } },
+    { 0,                            XF86XK_MonBrightnessDown,   spawn,  {.v = brdowncmd} },
+
 };
 
 /* button definitions */
