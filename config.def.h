20778d5bbeafe6d304f32850728fb58eb6a57180
Ali Bozorgzadeh <abzgzh@gmail.com>
2020-12-24 06:06:33 +0330

    Fix class for alcritty to use swallow

--- config.def.h
+++ config.def.h
@@ -31,7 +31,7 @@ static const Rule rules[] = {
     { "Gimp",           NULL,     NULL,           0,         1,          0,           0,        -1 },
     { "firefox",        NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
     { "Brave-browser",  NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-    { "St",             NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "Alacritty",             NULL,     NULL,           0,         0,          1,           0,        -1 },
     { NULL,             NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
