16c376e077a6afdbc28492e96a7e9ffa707e1746
Ali Bozorgzadeh <abzgzh@gmail.com>
2021-01-23 22:04:18 +0330

    config.def.h: Increase the emoji size

--- config.def.h
+++ config.def.h
@@ -19,7 +19,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=11:antialias=true:autohint=true" };
 static const char dmenufont[]           = "monospace:size=10";
 static const char normbgcolor[]         = "#222222";
 static const char normbordercolor[]     = "#444444";
