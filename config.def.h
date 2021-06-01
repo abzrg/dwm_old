fe12473361dfa093cfb43721f0645b21d633e811
Ali Bozorgzadeh <abzgzh@gmail.com>
2021-06-01 20:08:03 +0430

    Cleaning the code!

--- config.def.h
+++ config.def.h
@@ -3,24 +3,25 @@
 /* Constants */
 #define TERMINAL "st"
 #define TERMCLASS "St"
+#define TELEGRAM "telegram-desktop"
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const unsigned int borderpx  = 2;      /* border pixel of windows */
+static const unsigned int snap      = 32;     /* snap pixel */
+static const unsigned int gappih    = 10;     /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;     /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;     /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;     /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 1;      /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2; /* systray spacing */
+static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "inconsolata:size=14", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]           = "inconsolata:size=14";
+static const char *fonts[]          = { "mono:size=14", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static const char dmenufont[]           = "mono:size=14";
 static const char normbgcolor[]         = "#222222";
 static const char normbordercolor[]     = "#444444";
 static const char normfgcolor[]         = "#bbbbbb";
@@ -40,14 +41,14 @@ typedef struct {
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "90x15", NULL };
 const char *spcmd2[] = {"st", "-n", "spcalc", "-f", "monospace:size=20", "-g", "50x15", "-e", "bc", "-lq", NULL };
-const char *spcmd3[] = {"telegram-desktop", NULL };
-const char *spcmd4[] = {"st", "-n", "spncmpcpp", "-g", "110x20", "-e", "ncmpcpp", NULL };
+const char *spcmd3[] = {"st", "-n", "spncmpcpp", "-g", "110x20", "-e", "ncmpcpp", NULL };
+const char *spcmd4[] = {TELEGRAM, NULL };
 static Sp scratchpads[] = {
     /* name                  cmd  */
-    {"spterm",              spcmd1},
-    {"spcalc",              spcmd2},
-    {"sptlgrm",             spcmd3},
-    {"spncmpcpp",           spcmd4},
+    {"spterm",               spcmd1},
+    {"spcalc",               spcmd2},
+    {"spncmpcpp",            spcmd3},
+    {"sptlgram",             spcmd4},
 };
 
 /* tagging */
@@ -65,12 +66,13 @@ static const Rule rules[] = {
     { "Brave-browser",  NULL,     NULL,           1 << 8,    0,         0,          0,          -1,        -1 },
     { TERMCLASS,        NULL,     NULL,           0,         0,         0,          1,           0,        -1 },
     { "st-256color",    NULL,     NULL,           0,         0,         0,          1,           0,        -1 },
-    { NULL,             NULL,     "Event Tester", 0,         0,         0,          0,           1,        -1 }, /* xev */
+    // xev
+    { NULL,             NULL,     "Event Tester", 0,         0,         0,          0,           1,        -1 },
     // Scratchpads
     { NULL,             "spterm",       NULL,     SPTAG(0),  1,         1,          1,           0,        -1 },
     { NULL,             "spcalc",       NULL,     SPTAG(1),  1,         1,          1,           0,        -1 },
-    { NULL,             "telegram-desktop",     NULL,     SPTAG(2),  1,         1,          0,           0,        -1 },
-    { NULL,             "spncmpcpp",    NULL,     SPTAG(3),  1,         1,          1,           0,        -1 },
+    { NULL,             "spncmpcpp",    NULL,     SPTAG(2),  1,         1,          1,           0,        -1 },
+    { NULL,             TELEGRAM,       NULL,     SPTAG(3),  1,         1,          0,           0,        -1 },
 };
 
 /* layout(s) */
@@ -102,123 +104,108 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
-static const char *web[]  = { "brave", NULL };
-static const char *mail[]  = { "thunderbird", NULL };
-static const char *filemgr[]  = { "pcmanfm", NULL };
-
-/* multimedia keys */
-// static const char *upvol[]     = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%;",     NULL };
-// static const char *downvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%;",     NULL };
-// static const char *mutevol[]   = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
-// static const char *brupcmd[]   = { "/usr/bin/xbacklight", "-inc", "10",NULL };
-// static const char *brdowncmd[] = { "/usr/bin/xbacklight", "-dec", "10",NULL };
-
-/* playerctl */
-static const char *pctlpp[] = { "/usr/bin/playerctl", "--player=mpd", "play-pause", NULL };
-static const char *pctln[]  = { "/usr/bin/playerctl", "--player=mpd", "next",       NULL };
-static const char *pctlp[]  = { "/usr/bin/playerctl", "--player=mpd", "previous",   NULL };
-
-/* protonvpn */
-static const char *pvpn[]  = { "/home/ali/.local/scripts/Vpn", NULL };
-
+static const char *dmenucmd[] = {
+    "dmenu_run",
+    "-m", dmenumon,
+    "-fn", dmenufont,
+    "-nb", normbgcolor,
+    "-nf", normfgcolor,
+    "-sb", selbgcolor,
+    "-sf", selfgcolor,
+    NULL
+};
+static const char *termcmd[] = { "st", NULL };
+static const char *web[] = { "brave", NULL };
+static const char *mail[] = { "thunderbird", NULL };
+static const char *filemgr[] = { "pcmanfm", NULL };
+static const char *pctlpp[] = {
+    "/usr/bin/playerctl", "--player=mpd", "play-pause", NULL
+};
+static const char *pctln[] = {
+    "/usr/bin/playerctl", "--player=mpd", "next", NULL
+};
+static const char *pctlp[] = {
+    "/usr/bin/playerctl", "--player=mpd", "previous", NULL
+};
+static const char *pvpn[] = {
+    "/home/ali/.local/scripts/Vpn", NULL
+};
 
 static Key keys[] = {
-    /* modifier                     key        function        argument */
-    { ALT,                          XK_Escape, spawn,          SHCMD("toglayout;pkill -RTMIN+2 dwmblocks") },
-    { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-    { MODKEY,                       XK_e,      spawn,          SHCMD("emacsclient -c") },
-    { MODKEY,                       XK_n,      spawn,          SHCMD("$TERMINAL -e newsboat") },
-    { MODKEY,                       XK_r,      spawn,          SHCMD("$TERMINAL -e lf") },
-    { MODKEY,                       XK_w,      spawn,          {.v = web    } },
-    { MODKEY|ShiftMask,             XK_e,      spawn,          {.v = mail   } },
-    { MODKEY,                       XK_p,      spawn,          {.v = pctlpp } },
-    { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = filemgr } },
-    { MODKEY|ShiftMask,             XK_v,      spawn,          {.v = pvpn   } },
+    /* modifier            key        function        argument */
+    { MODKEY,              XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,              XK_d,      spawn,          {.v = dmenucmd } },
+    { MODKEY,              XK_w,      spawn,          {.v = web     }},
+    { MODKEY|ShiftMask,    XK_e,      spawn,          {.v = mail    }},
+    { MODKEY,              XK_p,      spawn,          {.v = pctlpp  }},
+    { MODKEY|ShiftMask,    XK_p,      spawn,          {.v = filemgr }},
+    { MODKEY|ShiftMask,    XK_v,      spawn,          {.v = pvpn    }},
+    { MODKEY,              XK_comma,   spawn,         {.v = pctlp   }},
+    { MODKEY,              XK_period,  spawn,         {.v = pctln   }},
+    { MODKEY,              XK_j,      focusstack,     {.i = +1   }},
+    { MODKEY,              XK_k,      focusstack,     {.i = -1   }},
+    { MODKEY,              XK_i,      incnmaster,     {.i = +1   }},
+    { MODKEY|ShiftMask,    XK_i,      incnmaster,     {.i = -1   }},
+    { MODKEY,              XK_0,      view,           {.ui = ~0  }},
+    { MODKEY|ShiftMask,    XK_0,      tag,            {.ui = ~0  }},
+    { MODKEY,              XK_h,      setmfact,       {.f = -0.05}},
+    { MODKEY,              XK_l,      setmfact,       {.f = +0.05}},
+    { MODKEY,              XK_t,      setlayout,      {.v = &layouts[0]}},
+    { MODKEY,              XK_y,      setlayout,      {.v = &layouts[1]}},
+    { MODKEY,              XK_u,      setlayout,      {.v = &layouts[2]}},
+    { MODKEY|ShiftMask,    XK_space,  togglefloating, {0}},
+    { MODKEY,              XK_s,      togglesticky,   {0}},
+    { MODKEY,              XK_f,      togglefullscr,  {0}},
+    { MODKEY,              XK_b,      togglebar,      {0}},
+    { MODKEY,              XK_space,  zoom,           {0}},
+    { MODKEY,              XK_Tab,    view,           {0}},
+    { MODKEY,              XK_q,      killclient,     {0}},
+    { MODKEY|ShiftMask,    XK_r,      quit,           {0}},
 
-    { MODKEY,                       XK_comma,   spawn,      {.v = pctlp  } },
-    // { MODKEY|ShiftMask,              XK_comma,   spawn,      SHCMD("mpc seek 0%") },
-    { MODKEY,                       XK_period,  spawn,      {.v = pctln  } },
-    // { MODKEY|ShiftMask,              XK_period,  spawn,      SHCMD("mpc repeat") },
-
-    { MODKEY,                       XK_b,      togglebar,      {0} },
-    { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-    { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-    { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = -1 } },
-    { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-    { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-
-    // Vanity-gaps
-    { MODKEY,                       XK_z,      incrgaps,       {.i = +3 } },
-    { MODKEY,                       XK_x,      incrgaps,       {.i = -3 } },
-    { MODKEY,                       XK_g,      togglegaps,     {0} },
-    { MODKEY|ShiftMask,             XK_g,      defaultgaps,    {0} },
-    // { MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-    // { MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-    // { MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-    // { MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-    // { MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-    // { MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-    // { MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-    // { MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-    // { MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-    // { MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-    // { MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-    // { MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
-
-    { MODKEY,                       XK_space,  zoom,           {0} },
-    { MODKEY,                       XK_Tab,    view,           {0} },
-    { MODKEY,                       XK_q,      killclient,     {0} },
-    { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-    { MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} },
-    { MODKEY,                       XK_u,      setlayout,      {.v = &layouts[2]} },
-    //{ MODKEY,                       XK_space,  setlayout,      {0} },
-    { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-    { MODKEY,                       XK_s,      togglesticky,   {0} },
-    { MODKEY,                       XK_f,      togglefullscr,  {0} },
-    { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-    { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+    // VanitygapS
+    { MODKEY,              XK_z,      incrgaps,       {.i = +3 } },
+    { MODKEY,              XK_x,      incrgaps,       {.i = -3 } },
+    { MODKEY,              XK_g,      togglegaps,     {0} },
+    { MODKEY|ShiftMask,    XK_g,      defaultgaps,    {0} },
 
     // Multi-screen setting (see the man page)
-    { MODKEY,                       XK_Left,    focusmon,   {.i = -1 } },
-    { MODKEY|ShiftMask,             XK_Left,    tagmon,     {.i = -1 } },
-    { MODKEY,                       XK_Right,   focusmon,   {.i = +1 } },
-    { MODKEY|ShiftMask,             XK_Right,   tagmon,     {.i = +1 } },
+    { MODKEY,              XK_Left,    focusmon,   {.i = -1 } },
+    { MODKEY|ShiftMask,    XK_Left,    tagmon,     {.i = -1 } },
+    { MODKEY,              XK_Right,   focusmon,   {.i = +1 } },
+    { MODKEY|ShiftMask,    XK_Right,   tagmon,     {.i = +1 } },
 
     // Scratchpads
-    { MODKEY|ShiftMask,             XK_Return, togglescratch,  {.ui = 0 } },
-    { MODKEY,                       XK_c,      togglescratch,  {.ui = 1 } },
-    { MODKEY|ShiftMask,             XK_m,      togglescratch,  {.ui = 2 } },
-    { MODKEY,                       XK_m,      togglescratch,  {.ui = 3 } },
+    { MODKEY|ShiftMask,    XK_Return, togglescratch,  {.ui = 0 } },
+    { MODKEY,              XK_c,      togglescratch,  {.ui = 1 } },
+    { MODKEY,              XK_m,      togglescratch,  {.ui = 2 } },
+    { MODKEY|ShiftMask,    XK_m,      togglescratch,  {.ui = 3 } },
 
-    TAGKEYS(                        XK_1,                      0)
-    TAGKEYS(                        XK_2,                      1)
-    TAGKEYS(                        XK_3,                      2)
-    TAGKEYS(                        XK_4,                      3)
-    TAGKEYS(                        XK_5,                      4)
-    TAGKEYS(                        XK_6,                      5)
-    TAGKEYS(                        XK_7,                      6)
-    TAGKEYS(                        XK_8,                      7)
-    TAGKEYS(                        XK_9,                      8)
+    TAGKEYS( XK_1, 0)
+    TAGKEYS( XK_2, 1)
+    TAGKEYS( XK_3, 2)
+    TAGKEYS( XK_4, 3)
+    TAGKEYS( XK_5, 4)
+    TAGKEYS( XK_6, 5)
+    TAGKEYS( XK_7, 6)
+    TAGKEYS( XK_8, 7)
+    TAGKEYS( XK_9, 8)
 
-    // Reload dwm
-    { MODKEY|ShiftMask,             XK_r,      quit,           {0} },
+    { ALT,                 XK_Escape, spawn,          SHCMD("toglayout;pkill -RTMIN+2 dwmblocks") },
+    { MODKEY,              XK_e,      spawn,          SHCMD("emacsclient -c"                    ) },
+    { MODKEY,              XK_n,      spawn,          SHCMD("$TERMINAL -e newsboat"             ) },
+    { MODKEY,              XK_r,      spawn,          SHCMD("$TERMINAL -e lf"                   ) },
 
     // Media and brightness keys
-    { 0,                            XF86XK_AudioLowerVolume,    spawn,  SHCMD("pactl set-sink-volume 0 -5%; pkill -RTMIN+3 dwmblocks;") },
-    { 0,                            XF86XK_AudioMute,           spawn,  SHCMD("pactl set-sink-mute 0 toggle; pkill -RTMIN+3 dwmblocks;") },
-    { 0,                            XF86XK_AudioRaiseVolume,    spawn,  SHCMD("pactl set-sink-volume 0 +5%; pkill -RTMIN+3 dwmblocks;") },
-    { 0,                            XF86XK_MonBrightnessUp,     spawn,  SHCMD("xbacklight -inc 10 ; pkill -RTMIN+4 dwmblocks") },
-    { 0,                            XF86XK_MonBrightnessDown,   spawn,  SHCMD("xbacklight -dec 10 ; pkill -RTMIN+4 dwmblocks") },
-    { MODKEY,                       XK_F1,                      spawn,  SHCMD("pactl set-sink-mute 0 toggle; pkill -RTMIN+3 dwmblocks;") },
-    { MODKEY,                       XK_F2,                      spawn,  SHCMD("pactl set-sink-volume 0 -5%; pkill -RTMIN+3 dwmblocks;") },
-    { MODKEY,                       XK_F3,                      spawn,  SHCMD("pactl set-sink-volume 0 +5%; pkill -RTMIN+3 dwmblocks;") },
-    { MODKEY,                       XK_F11,                     spawn,  SHCMD("xbacklight -dec 10 ; pkill -RTMIN+4 dwmblocks") },
-    { MODKEY,                       XK_F12,                     spawn,  SHCMD("xbacklight -inc 10 ; pkill -RTMIN+4 dwmblocks") },
-
+    { 0,      XF86XK_AudioLowerVolume,  spawn, SHCMD("pactl set-sink-volume 0 -5%; pkill -RTMIN+3 dwmblocks;") },
+    { 0,      XF86XK_AudioMute,         spawn, SHCMD("pactl set-sink-mute 0 toggle; pkill -RTMIN+3 dwmblocks;") },
+    { 0,      XF86XK_AudioRaiseVolume,  spawn, SHCMD("pactl set-sink-volume 0 +5%; pkill -RTMIN+3 dwmblocks;") },
+    { 0,      XF86XK_MonBrightnessUp,   spawn, SHCMD("xbacklight -inc 10 ; pkill -RTMIN+4 dwmblocks") },
+    { 0,      XF86XK_MonBrightnessDown, spawn, SHCMD("xbacklight -dec 10 ; pkill -RTMIN+4 dwmblocks") },
+    { MODKEY, XK_F1,                    spawn, SHCMD("pactl set-sink-mute 0 toggle; pkill -RTMIN+3 dwmblocks;") },
+    { MODKEY, XK_F2,                    spawn, SHCMD("pactl set-sink-volume 0 -5%; pkill -RTMIN+3 dwmblocks;") },
+    { MODKEY, XK_F3,                    spawn, SHCMD("pactl set-sink-volume 0 +5%; pkill -RTMIN+3 dwmblocks;") },
+    { MODKEY, XK_F11,                   spawn, SHCMD("xbacklight -dec 10 ; pkill -RTMIN+4 dwmblocks") },
+    { MODKEY, XK_F12,                   spawn, SHCMD("xbacklight -inc 10 ; pkill -RTMIN+4 dwmblocks") },
 };
 
 /* button definitions */
@@ -237,4 +224,3 @@ static Button buttons[] = {
     { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
     { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
