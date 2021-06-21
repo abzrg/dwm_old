0beab3c9e987597b836e7bd80ac9e4f703c0265e
Ali Bozorgzadeh <abzgzh@gmail.com>
2021-06-21 19:51:27 +0430

    Add Xresources patch; Format code; Change x11 path (config.mk)

--- config.def.h
+++ config.def.h
@@ -6,45 +6,46 @@
 #define TELEGRAM "telegram-desktop"
 
 /* appearance */
-static const unsigned int borderpx  = 2;      /* border pixel of windows */
-static const unsigned int snap      = 32;     /* snap pixel */
-static const unsigned int gappih    = 10;     /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;     /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;     /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;     /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;      /* 1 means no outer gap when there is only one window */
-static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2; /* systray spacing */
-static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "sans:size=14", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
-static const char dmenufont[]           = "sans:size=14";
-static const char normbgcolor[]         = "#222222";
-static const char normbordercolor[]     = "#444444";
-static const char normfgcolor[]         = "#bbbbbb";
-static const char selfgcolor[]          = "#eeeeee";
-static const char selbordercolor[]      = "#770000";
-static const char selbgcolor[]          = "#005577";
-static const char *colors[][3]      = {
-    /*               fg         bg         border   */
+static unsigned int borderpx       = 0;  /* border pixel of windows */
+static unsigned int snap           = 32; /* snap pixel */
+static const unsigned int gappih         = 10; /* horiz inner gap between windows */
+static const unsigned int gappiv         = 10; /* vert inner gap between windows */
+static const unsigned int gappoh         = 10; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov         = 10; /* vert outer gap between windows and screen edge */
+static const int smartgaps               = 0;  /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;  /* systray spacing */
+static const int systraypinningfailfirst = 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;  /* 0 means no systray */
+static const int swallowfloating         = 0;  /* 1 means swallow floating windows by default */
+static int showbar                       = 1;  /* 0 means no bar */
+static int topbar                        = 1;  /* 0 means bottom bar */
+static char dwmfont[]                    = "sans:size=14";
+static char dmenufont[]                  = "sans:size=14";
+static char *fonts[]                     = { dwmfont, "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static char normbgcolor[]                = "#222222";
+static char normbordercolor[]            = "#444444";
+static char normfgcolor[]                = "#bbbbbb";
+static char selfgcolor[]                 = "#eeeeee";
+static char selbordercolor[]             = "#770000";
+static char selbgcolor[]                 = "#005577";
+static char *colors[][3]                 = {
+    /*               fg           bg           border          */
     [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-    [SchemeSel]  = { selfgcolor, selbgcolor,  selbordercolor   },
+    [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
-// Scratchpads
+/* Scratchpads */
 typedef struct {
     const char *name;
     const void *cmd;
 } Sp;
-const char *spcmd1[] = {"st", "-n", "spterm", "-g", "90x15", NULL };
-const char *spcmd2[] = {"st", "-n", "spcalc", "-f", "monospace:size=20", "-g", "50x15", "-e", "bc", "-lq", NULL };
-const char *spcmd3[] = {"st", "-n", "spncmpcpp", "-g", "110x20", "-e", "ncmpcpp", NULL };
-const char *spcmd4[] = {TELEGRAM, NULL };
+const char *spcmd1[]    = {"st", "-n", "spterm", "-g", "90x23", NULL };
+const char *spcmd2[]    = {"st", "-n", "spcalc", "-f", "monospace:size=20", "-g", "50x15", "-e", "bc", "-lq", NULL };
+const char *spcmd3[]    = {"st", "-n", "spncmpcpp", "-g", "110x20", "-e", "ncmpcpp", NULL };
+const char *spcmd4[]    = {TELEGRAM, NULL };
 static Sp scratchpads[] = {
-    /* name                  cmd  */
+    /* name                  cmd   */
     {"spterm",               spcmd1},
     {"spcalc",               spcmd2},
     {"spncmpcpp",            spcmd3},
@@ -76,9 +77,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
+static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
@@ -129,6 +130,27 @@ static const char *pctlp[] = {
 };
 static const char *pvpn[] = {
     "/home/ali/.local/scripts/Vpn", NULL
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+        { "dwmfont",            STRING,  &dwmfont },
+        { "dmenufont",          STRING,  &dmenufont },
+        { "normbgcolor",        STRING,  &normbgcolor },
+        { "normbordercolor",    STRING,  &normbordercolor },
+        { "normfgcolor",        STRING,  &normfgcolor },
+        { "selbgcolor",         STRING,  &selbgcolor },
+        { "selbordercolor",     STRING,  &selbordercolor },
+        { "selfgcolor",         STRING,  &selfgcolor },
+        { "borderpx",           INTEGER, &borderpx },
+        { "snap",               INTEGER, &snap },
+        { "showbar",            INTEGER, &showbar },
+        { "topbar",             INTEGER, &topbar },
+        { "nmaster",            INTEGER, &nmaster },
+        { "resizehints",        INTEGER, &resizehints },
+        { "mfact",              FLOAT,   &mfact },
 };
 
 static Key keys[] = {
